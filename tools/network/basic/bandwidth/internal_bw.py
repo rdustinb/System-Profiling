6fc2ec71fb872e7242c310b342b45ae13411faed
Dustin Brothers <gentian.limited-0t@icloud.com>
2025-08-28 16:30:13 -0600

    Added a block to normalize the data structure and keep only the bandwidth numbers. Averaging and upper/lower bound number processing is next...

--- tools/network/basic/bandwidth/internal_bw.py
+++ tools/network/basic/bandwidth/internal_bw.py
@@ -1,3 +1,8 @@
+DEBUG_PRINT_STEP0 = False
+DEBUG_PRINT_STEP1 = False
+DEBUG_PRINT_STEP2 = True
+DEBUG_PRINT_STEP3 = False
+
 ################################
 # Get the configuration
 ################################
@@ -12,6 +17,10 @@ config.read('config.ini')
 # Access values from the configuration file
 theseNodes = config.get('main', 'nodes').split()
 
+# DEBUG
+if DEBUG_PRINT_STEP0:
+  print(theseNodes)
+
 ################################
 # iPerf3 All Nodes
 ################################
@@ -23,8 +32,9 @@ theseRawResults = dict()
 # For each node, simply get the output from iPerf3 with some minor filtering
 for thisNode in theseNodes:
   try:
+    print("Capturing network segment bandwidth to node %s"%(thisNode))
     cmdResultByteArray = subprocess.check_output(
-      "iperf3 -c %s | grep \"sec\""%thisNode,
+      "iperf3 -f m -c %s | grep \"sec\""%thisNode,
       shell = True,
       executable = "/bin/bash",
       stderr = subprocess.STDOUT
@@ -33,15 +43,29 @@ for thisNode in theseNodes:
   except:
     print("An error occured while transferring data with %s"%thisNode)
 
-print(theseRawResults)
+# DEBUG
+if DEBUG_PRINT_STEP1:
+  print(theseRawResults)
 
 ################################
 # Pull Out the Data we want
 ################################
 
-# String Shape
-# [ 7] 0.00-1.01 sec 116 MBytes 971 Mbits/sec
-# Split Indices
-# 0  1         2   3   4      5   6         7
+# String Shape:   [ 7] 0.00-1.01 sec 116 MBytes 971 Mbits/sec
+# Split Indices:  0  1         2   3   4      5   6         7
+
+theseNodeBWLists = dict()
+
 for thisNode, theseNodeValues in theseRawResults.items():
+  thisBWList = list()
+  # Pull out only the BW value
   for thisNodeValue in theseNodeValues:
+    thisBWList.append(thisNodeValue.split()[6])
+  # Put the list under the dict key, strip out the last two values since they are average values
+  theseNodeBWLists[thisNode] = thisBWList[:-2]
+
+# DEBUG
+if DEBUG_PRINT_STEP2:
+  for thisNode, theseNodeValues in theseNodeBWLists.items():
+    print("The node %s has the BW sample points:"%(thisNode))
+    print(theseNodeValues)
