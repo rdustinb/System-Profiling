7b121962d90968e92d886314dc0e26f2eff9e6cf
Dustin Brothers <gentian.limited-0t@icloud.com>
2025-10-23 08:30:12 -0600

    Adding some more control to the iperf3 commend.

--- tools/network/basic/bandwidth/support/funcs.py
+++ tools/network/basic/bandwidth/support/funcs.py
@@ -4,11 +4,39 @@ def callIperf3(thisNode, theseRawResults):
 
   print("Capturing network segment bandwidth to node %s"%(thisNode))
 
+  testBidirectional = True
+  testType = "bytes" # time, bytes, or packets
+  testCount = "20" # any number
+  testUnit = "G" # K, M, G, or T
+
   thisNodeRaw = list()
 
+  if testType == "time":
+    thisTestVariant = f"-t {testCount}"
+  elif testType == "bytes":
+    thisTestVariant = f"-n {testCount}{testUnit}"
+  elif testType == "packets":
+    thisTestVariant = f"-k {testCount}{testUnit}"
+
+  if testBidirectional:
+    thisBidi = "--bidi"
+  else:
+    thisBidi = ""
+
+  # Mutually Exclusive Options:
+  #
+  # Add -t X to specify the number of time in seconds to transmit
+  # Add -n X[KMGT] to specify the number of bytes to transmit
+  # Add -k X[KMGT] to specify the number of packets to transmit
+  # Add -l X[KMGT] to specify the length of buffer to transmit
+  #
+  # Extra Options:
+  #
+  # Add --bidir to flow data in both directions
+  #
   try:
     cmdResultByteArray = subprocess.check_output(
-      "iperf3 -f m -c %s | grep \"sec\""%thisNode,
+      f"iperf3 -f m {thisTestVariant} {thisBidi} -c {thisNode} | grep \"sec\"",
       shell = True,
       executable = "/bin/bash",
       stderr = subprocess.STDOUT
